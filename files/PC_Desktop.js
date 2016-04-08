d38f440b215ae07ea86d0820cf6fabcec401accb
Sebastiao Jose Da Costa Reis Perestrello De Vasconcellos (R2014525) <r2014525@isegi.unl.pt>
2016-04-08 15:32:55 +0100

    Automatic Resizing The files now automatically detect the size of your screen and adapt accordingly

--- files/PC_Desktop.js
+++ files/PC_Desktop.js
@@ -1,5 +1,39 @@
 //Currently empty. Type in functions as you need them created
-function toggleAppearance()
+function stopTime()
 {
-	alert("Hai");
+	//This function should send a message to the server to stop the time.
+	console.log("Time stopped");
+}
+
+function startTime($speed)
+{
+	console.log("Game speed changed to: " + $speed);
+	if($speed == 1)
+	{
+		//This result should send a message to the server to advance time at normal speed
+	}
+	else if($speed == 2)
+	{
+		//This result should send a message to the server to advance time at x2 speed (or more)
+	}
+	else if($speed == 3)
+	{
+		//This function should send a message to the server to advance time at x3 speed (or more)
+	}
+	else
+	{
+		console.log("Failed. You tried to change the game speed with the number: " + $speed);
+	}
+}
+
+function isPlayerConnected()
+{
+	//This function requests the $isConnected flag from the server and returns its value.
+	console.log("Returning the state of $isConnected");
+	//return $isConnected;
+}
+
+if(isPlayerConnected())
+{
+	$("#currentconnection").toggle()
 }
