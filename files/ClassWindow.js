f15a1b8ad4ea6b4027086b49c5a051ad4b193163
99Ranged <olebenfica-vermelho@hotmail.com>
2016-05-03 16:59:53 +0100

    Closing and opening back windows now works
    We can close and open back windows by playing with the css, chaging the
    div to "hidden" and then using a funtion to set the style to "visible"

--- files/ClassWindow.js
+++ files/ClassWindow.js
@@ -7,24 +7,31 @@ class Window {
 	var bgColour = bgColour;
 	var fgColour = fgColour;
 	this.html = ' \
-		<div class="window" data-role="draggable" style="width: ' + windowWidth + 'px; position: relative; z-index: 1; top: 0%; left: 0%"> \
+		<div id="'+ windowName + '" class="window" data-role="draggable" style="width: ' + windowWidth + 'px; position: relative; z-index: 1; top: 0%; left: 0%"> \
 			<div class="window-caption">\
 				<span class="window-caption-icon">\
 					<span class="' + windowIcon + '"></span>\
 				</span>\
 				<span class="window-caption-title" style="float: left;">' + windowName + '</span>\
 				<span class="btn-min" style="float:right;"></span>\
-				<span class="btn-max" style="float:right;"></span>\
-				<span class="btn-close" style="float:right;"></span>\
+				<span class="btn-max" style="float:right;" ></span>\
+				<span class="btn-close" style="float:right;" onclick="this.parentNode.parentNode.style.visibility=&quot;hidden&quot;"></span>\
 			</div>\
 			<div class="window-content bg-' + bgColour + ' fg-' + fgColour + '" style="height: ' + windowHeight + 'px;">\
 				<p style="float: left;"></p>\
 			</div>\
 		</div>';
+		
+	this.vis = function()
+	{
+	 document.getElementById(windowName).style.visibility='visible';
+	}
   }
   
   windowHtml()
   {
 	  return this.html;
   }
+  
+  
 }
